b2efeb1ec75b0ecd75773274ff7fdedd007e77f9
Nicolas Marchildon <nicolas@marchildon.net>
2013-10-09 22:17:54 -0400

    Query PostgreSQL

---
+++ README.md
@@ -0,0 +1,28 @@
+# evalmtl.js
+
+Web frontend for Évaluations foncières de Montréal.
+
+## 1. Scrape and export to PostgreSQL
+
+Follow instructions at https://github.com/elecnix/evalmtl
+
+# 2. Create streets table
+
+    $ psql -d evalmtl
+    evalmtl=# create table streets as select distinct street_name from evaluations_2014 order by street_name;
+
+## 3. Install node modules, assuming NPM is already installed:
+
+    npm install
+
+## 4. Run!
+
+    DATABASE_URL=postgres://user:pass@localhost/evalmtl node app
+
+Or push to Heroku:
+
+    heroku create
+    heroku addons:add heroku-postgresql:dev
+    git push heroku master
+    heroku open
+
--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 app.use(express.logger());
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('<a href="/streets">Streets</a> <a href="/properties">Properties</a>');
+});
+app.get('/properties', function(req, res){
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if(err) return console.error(err);
+    client.query('SELECT * FROM evaluations_2014 limit 10', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      res.send(result.rows);
+    });
+  });
+});
+app.get('/streets', function(req, res){
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if(err) {
+      res.send(err);
+    }
+    client.query('SELECT * FROM streets', function(err, result) {
+      done();
+      if(err) {
+        return res.send(err);
+      }
+      res.send(result.rows);
+    });
+  });
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
