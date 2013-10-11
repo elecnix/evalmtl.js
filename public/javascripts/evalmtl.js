9550b85404d2c6eb24006b7a5e7c1f434b8ab60a
Nicolas Marchildon <nicolas@marchildon.net>
2013-10-10 22:20:01 -0400

    Spinner while loading streets

--- public/javascripts/evalmtl.js
+++ public/javascripts/evalmtl.js
@@ -1,9 +1,14 @@
 angular.module('evalmtl', ['restangular']).
   controller('EvalMtlCtrl', function($scope, Restangular) {
+    $scope.loaded = true;
     $scope.streets = [];
     $scope.search = function() {
       if ($scope.q_street.length < 2) return $scope.streets = [];
-      $scope.streets = Restangular.all('streets').getList({q: $scope.q_street});
+      $scope.loaded = false;
+      Restangular.all('streets').getList({q: $scope.q_street}).then(function(streets) {
+        $scope.loaded = true;
+        $scope.streets = streets;
+      });
     };
   });
 
